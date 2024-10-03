a4536e5d1dbbd42f7ed70de4ea9e448cde2e4cc7
AzadAshurov <azadva-bp217@code.edu.az>
2024-10-03 10:49:39 +0400

    Update Program.cs

--- HOMEWORK/TaskTwo/Program.cs
+++ HOMEWORK/TaskTwo/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.WriteLine("Enter your number");
 int num = Convert.ToInt32(Console.ReadLine());
 bool prime = true;
-for (int i = 2; i < (num / 2); i++)
+for (int i = 2; i <= (num / 2); i++)
 {
     if (num % i == 0)
     {
